3667602b4883697d269f6a1d1be25abd7716b033
kriyona infotech <kriyonainfotech@gmail.com>
2026-04-12 10:30:54 +0530

    save

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3333/api';
+const API_BASE_URL = 'http://lotus-backend-nine.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
