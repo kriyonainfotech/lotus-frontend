9e8824f57e8038cf51e683ab4ccf9458561f40b1
kriyona infotech <kriyonainfotech@gmail.com>
2026-04-12 10:36:39 +0530

    fix tailwind deps for vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://lotus-backend-nine.vercel.app/api';
+const API_BASE_URL = 'https://lotus-backend-nine.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
